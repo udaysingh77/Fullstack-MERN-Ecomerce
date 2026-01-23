4f3d5d26becab9317b5e9e3597947581a7a4b0ba
udaysingh77 <160723117+udaysingh77@users.noreply.github.com>
2026-01-23 17:58:24 +0530

    feat: initialize frontend with Vite and add Tailwind CSS setup
    - Created package.json for Vite project with necessary dependencies and scripts.
    - Updated App component to include a normal button and a custom Button component from the UI library.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from '@/components/ui/button';
 const App = () => {
   return (
     <div >
+      <button>Normal button</button>
+      <Button>Advance Buttion</Button>
       Hello World
     </div>
   )
