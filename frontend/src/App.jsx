04ed697efdc9c28144a29a72f48960b6814c40d4
udaysingh77 <160723117+udaysingh77@users.noreply.github.com>
2026-01-23 19:24:36 +0530

    feat: add routing and create basic page components for Home, Signup, and Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Button } from '@/components/ui/button';
+import { createBrowserRouter, RouterProvider  } from "react-router-dom"
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<><Navbar></Navbar><Home></Home></>
+  }
+])
 const App = () => {
   return (
-    <div >
-      <button>Normal button</button>
-      <Button>Advance Buttion</Button>
-      Hello World
-    </div>
+    <>
+      <RouterProvider router={router}>
+        <Home></Home>
+        <Signup></Signup>
+        <Login></Login>
+      </RouterProvider>
+    </>
   )
 }
 
